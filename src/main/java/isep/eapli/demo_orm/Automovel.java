9cf82c9c3aece5073dc4d4f615c3e16e107ddce8
Tiago Botelho <1211720@isep.ipp.pt>
2025-02-25 17:21:13 +0000

    #7, #8

--- src/main/java/isep/eapli/demo_orm/Automovel.java
+++ src/main/java/isep/eapli/demo_orm/Automovel.java
@@ -2,6 +2,7 @@ package isep.eapli.demo_orm;
 
 public class Automovel {
     int numKms;
+    String matricula;
 
     public int getNumKms() {
         return numKms;
@@ -9,5 +10,21 @@ public class Automovel {
 
     public void setNumKms(int numKms) {
         this.numKms = numKms;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovel{" +
+                "numKms=" + numKms +
+                ", matricula='" + matricula + '\'' +
+                '}';
     }
 }
