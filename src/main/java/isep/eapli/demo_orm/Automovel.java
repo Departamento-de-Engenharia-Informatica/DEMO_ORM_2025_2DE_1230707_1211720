245a45cf0972ae2cdb69715874e2d8c570864997
joaoribeiro200 <1230707@isep.ipp.pt>
2025-02-25 17:14:48 +0000

    Criação do método setNumKms da Classe Automovel .

--- src/main/java/isep/eapli/demo_orm/Automovel.java
+++ src/main/java/isep/eapli/demo_orm/Automovel.java
@@ -1,4 +1,13 @@
 package isep.eapli.demo_orm;
 
 public class Automovel {
+    int numKms;
+
+    public int getNumKms() {
+        return numKms;
+    }
+
+    public void setNumKms(int numKms) {
+        this.numKms = numKms;
+    }
 }
